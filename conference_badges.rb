ec20afc8361e01c1920703d87e1f287d1ff73c34
bustling-clone-9138 <jtaylorjohnson@gmail.com>
2020-03-25 20:34:13 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -5,7 +5,7 @@ end
 speakers = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
 
 def batch_badge_creator(speakers)
-  batch_badge_creator.each do |speakers|
+  speakers.each do |speakers|
   puts "Hello, my name is #{speakers}."
   end
 end
