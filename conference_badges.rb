26f4e779947f0a0565218a4ff358be5a22a9cc7d
bustling-clone-9138 <jtaylorjohnson@gmail.com>
2020-03-25 21:19:43 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,28 @@
-# Write your code here.
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(speakers)
+  messages = []
+  speakers.each do |name|
+  messages << "Hello, my name is #{name}."
+  end
+messages
+end
+
+def assign_rooms(speakers)
+  rooms = []
+  speakers.each_with_index do |name, index|
+  rooms << "Hello, #{name}! You'll be assigned to room #{index+1}!"
+  end
+rooms
+end
+
+def printer(speakers)
+  batch_badge_creator(speakers).each do |messages|
+  puts messages
+  end
+  assign_rooms(speakers).each do |rooms|
+  puts rooms 
+  end
+end
